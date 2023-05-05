5a0f0a4fa6857592d9442beaff80d71ba8440c76
sophiamziou <sofiamziou@gmail.com>
2023-05-06 00:20:44 +0200

    snack 10 done

--- src/org/lessons/java/Snack10.java
+++ src/org/lessons/java/Snack10.java
@@ -1,5 +1,20 @@
 package org.lessons.java;
 
-public class Snack10 {
+import java.util.Scanner;
 
+public class Snack10 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("quanto è lungo il raggio?");
+		int r = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println("il raggio è di "+r+"cm");
+		System.out.println("------------");
+		
+		float Area = r * r * 3.14f;
+		System.out.println("l'area del cerchio è di "+Area+"cm2");
+		float Perimetro = 2 * r + 3.14f;
+		System.out.println("il perimetro del cerchio è di "+Perimetro+"cm");
+	}
 }
