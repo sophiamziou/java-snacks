e9f2b93711009a326afdd042b855bdc793154d2c
sophiamziou <sofiamziou@gmail.com>
2023-05-06 00:48:14 +0200

    snack 11 done

--- src/org/lessons/java/Snack11.java
+++ src/org/lessons/java/Snack11.java
@@ -1,5 +1,21 @@
 package org.lessons.java;
 
-public class Snack11 {
+import java.util.Scanner;
 
+public class Snack11 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("scegli un numero");
+		int num = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println("----------");
+		for (int i=1; i<=num/2; i++) {
+			
+			if (num % i == 0) {
+				System.out.println(i);
+			}
+		}
+		System.out.println(num);
+	}
 }
