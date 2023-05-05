5c2e310c3547a56cade7af56e528ad88e62ba840
sophiamziou <sofiamziou@gmail.com>
2023-05-06 00:15:59 +0200

    snack 9 done

--- src/org/lessons/java/Snack9.java
+++ src/org/lessons/java/Snack9.java
@@ -1,5 +1,22 @@
 package org.lessons.java;
 
-public class Snack9 {
+import java.util.Scanner;
 
+public class Snack9 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("quanto è lunga la base?");
+		int b = scanner.nextInt();
+		System.out.println("quanto è lunga l'altezza?");
+		int h = scanner.nextInt();
+		scanner.close();
+		System.out.println("la base è di "+b+"cm");
+		System.out.println("l'altezza è di "+h+"cm");
+		System.out.println("------------");
+		
+		int Area = b * h;
+		System.out.println("l'area del rettangolo è di "+Area+"cm2");
+		int Perimetro = 2*b + 2*h;
+		System.out.println("il perimetro del rettangolo è di "+Perimetro+"cm");
+	}
 }
