ac427da9f953e57e4b311fcfc60383ba091780a7
sophiamziou <sofiamziou@gmail.com>
2023-05-05 11:39:11 +0200

    Snack 3 done

--- src/org/lessons/java/Snack3.java
+++ src/org/lessons/java/Snack3.java
@@ -2,5 +2,13 @@ package org.lessons.java;
 
 public class Snack3 {
     public static void main(String[] args) {
+        int[] numeri = { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+
+        int somma = 0;
+        for (int i = 0; i < numeri.length; i += 2) {
+            somma += numeri[i];
+        }
+
+        System.out.println("La somma degli elementi in posizione dispari è: " + somma);
     }
 }
